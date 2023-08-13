ecbbd65d4283d2fb3a4086981fa408b249972578
Julio.S <just.julio95@gmail.com>
2023-08-12 23:10:21 -0400

    fixed a MAJOR issue that as going on with file structuring

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,37 +1,66 @@
 <?php
 
-if(isset($_GET['p_id'])){
+if (isset($_GET['p_id'])) {
   $edit_id = $_GET['p_id'];
 }
 
 $query = "SELECT * FROM posts WHERE post_id = {$edit_id}";
-    $select_posts_by_id = mysqli_query($connection, $query);
+$select_posts_by_id = mysqli_query($connection, $query);
 
-    while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
-      $post_id = $row['post_id'];
-      $post_author = $row['post_author'];
-      $post_title = $row['post_title'];
-      $post_cat = $row['post_category_id'];
-      $post_status = $row['post_status'];
-      $post_image = $row['post_image'];
-      $post_content = $row['post_content'];
-      $post_tags = $row['post_tags'];
-      $post_comments = $row['post_comment_count'];
-      $post_date = $row['post_date'];
-    }
+while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
+  $post_id = $row['post_id'];
+  $post_author = $row['post_author'];
+  $post_title = $row['post_title'];
+  $post_cat = $row['post_category_id'];
+  $post_status = $row['post_status'];
+  $post_image = $row['post_image'];
+  $post_content = $row['post_content'];
+  $post_tags = $row['post_tags'];
+  $post_comments = $row['post_comment_count'];
+  $post_date = $row['post_date'];
+}
+
+if(isset($_POST['update_post'])){
+  $post_title = $_POST['title'];
+  $post_author = $_POST['author'];
+  $post_category_id = $_POST['post_category'];
+  $post_status = $_POST['post_status'];
+  $post_image = $_FILES['image']['name'];
+  $post_image_temp = $_FILES['image']['tmp_name'];
+  $post_tags = $_POST['post_tags'];
+  $post_content = $_POST['post_content'];
+
+  move_uploaded_file($post_image_temp, "../images/$post_image");
+
+  $query = "UPDATE posts SET ";
+  $query .= "post_title = '{$post_title}', ";
+  $query .= "post_date = now(), ";
+  $query .= "post_author = '{$post_author}', ";
+  $query .= "post_category_id = '{$post_category_id}', ";
+  $query .= "post_status = '{$post_status}', ";
+  $query .= "post_image = '{$post_image}', ";
+  $query .= "post_tags = '{$post_tags}', ";
+  $query .= "post_content = '{$post_content}' ";
+  $query .= "WHERE post_id = '{$edit_id}'";
+
+  $update_post = mysqli_query($connection, $query);
+  
+  confirm($update_post);
+}
 
 ?>
 
 <form action="" method="post" enctype="multipart/form-data">
+  <?php echo "<image src='../images/image_5.jpg' alt=''></image>"?>
 
   <div class="form-group">
     <label for="title">Post Title</label>
-    <input value="<?php echo $post_title?>" type="text" class="form-control" name="title">
+    <input value="<?php echo $post_title ?>" type="text" class="form-control" name="title">
   </div>
 
   <div class="form-group">
     <label for="post_category">Post Category ID</label>
-    <select name="" id="">
+    <select name="post_category" id="">
       <?php
       $query = "SELECT * FROM categories";
       $select_categories = mysqli_query($connection, $query);
@@ -50,30 +79,31 @@ $query = "SELECT * FROM posts WHERE post_id = {$edit_id}";
 
   <div class="form-group">
     <label for="title">Post Author</label>
-    <input value="<?php echo $post_author?>" type="text" class="form-control" name="author">
+    <input value="<?php echo $post_author ?>" type="text" class="form-control" name="author">
   </div>
 
   <div class="form-group">
     <label for="post_status">Post Status</label>
-    <input value="<?php echo $post_status?>" type="text" class="form-control" name="post_status">
+    <input value="<?php echo $post_status ?>" type="text" class="form-control" name="post_status">
   </div>
 
   <div class="form-group">
-    <img width="100" src="../images/<?php echo $post_image?>" alt=""> <!-- why won't this work??? -->
+    <img width="100" src="../images/<?php echo $post_image ?>" alt="">
+    <input type="file" name="image" id="">
   </div>
 
   <div class="form-group">
     <label for="post_tags">Post Tags</label>
-    <input value="<?php echo $post_tags?>" type="text" class="form-control" name="post_tags">
+    <input value="<?php echo $post_tags ?>" type="text" class="form-control" name="post_tags">
   </div>
 
   <div class="form-group">
     <label for="post_content">Post Content</label>
-    <textarea value="<?php echo $post_content?>" class="form-control" name="post_content" cols="30" rows="10"></textarea>
+    <textarea value="<?php echo $post_content ?>" class="form-control" name="post_content" cols="30" rows="10"></textarea>
   </div>
 
   <div class="form-group">
-    <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+    <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
   </div>
 
 </form>
